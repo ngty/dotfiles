dfd7965b34a683e71275fa836f04251c66eadca0
NgTzeYang <ngty77@gmail.com>
2011-10-02 16:20:58 +0800

    Disabled plugins are stored in ./disabled_plugins, & actions are done only required.

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,31 +1,57 @@
 #!/bin/bash
 
-REPO_URL=https://github.com/scrooloose/vimfiles.git
 BASE_DIR=`dirname $0`
 BASE_DIR=`cd $BASE_DIR && pwd`
+REPO_URL=https://github.com/scrooloose/vimfiles.git
 REPO_DIR=${BASE_DIR}/`echo $REPO_URL | sed -e 's|.*/\(.*\).git|\1|'`
 
+# //////////////////////////////////////////////////////////////////
 # Install system dependencies
+# //////////////////////////////////////////////////////////////////
 source ${BASE_DIR}/../helpers
 source ${BASE_DIR}/platforms/$(platform)
 
+# //////////////////////////////////////////////////////////////////
 # Clone vimfiles repo & update submodules
-[ -d $REPO_DIR ] || git clone $REPO_URL
-cd $REPO_DIR
-git submodule init && git submodule update
+# //////////////////////////////////////////////////////////////////
+echo -n ">> Initializing base config from $REPO_URL ... "
 
+if [[ -d $REPO_DIR ]]; then
+  echo "skipped"
+else
+  echo
+  cd $BASE_DIR && git clone $REPO_URL
+  cd $REPO_DIR && git submodule init && git submodule update
+fi
+
+# //////////////////////////////////////////////////////////////////
 # Install extra plugins
+# //////////////////////////////////////////////////////////////////
 cd $REPO_DIR
 for url in `cat ${BASE_DIR}/extra_plugins`; do
+  echo -n ">> Cloning extra plugin $url ... "
   name=`echo $url | sed -e 's|.*/\(.*\).git|\1|'`
-  git clone $url bundle/${name}
+
+  if [ -d bundle/${name} ]; then
+    echo "skipped"
+  else
+    echo
+    git clone $url bundle/${name}
+  fi
 done
 
+# //////////////////////////////////////////////////////////////////
 # Remove not required plugins
+# //////////////////////////////////////////////////////////////////
 cd $REPO_DIR
-mv bundle/gundo bundle/.gundo.disabled
+for plugin in `cat ${BASE_DIR}/disabled_plugins`; do
+  [ -d bundle/${plugin} ] && \
+    mv bundle/${plugin} bundle/${plugin}.disabled
+done
 
-# Symlinking to vim path
+# //////////////////////////////////////////////////////////////////
+# Symlink to vim path
+# //////////////////////////////////////////////////////////////////
 rm ~/.vimrc && ln -s ${BASE_DIR}/vimrc ~/.vimrc
 rm ~/.vim && ln -s ${BASE_DIR}/vimfiles ~/.vim
 
