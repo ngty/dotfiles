6ee534d4c18306cac0336868cf995acc4a4321dd
Ng Tze Yang <ngty77@gmail.com>
2012-01-15 00:27:53 +0800

    Ensure tmux package is installed while doing setup.

--- etc/tmux/setup.sh
+++ etc/tmux/setup.sh
@@ -2,7 +2,8 @@
 DIR=`dirname ${BASH_SOURCE[0]}`
 DIR=`cd $DIR && pwd`
 
-source `dirname $0`/../helpers
+source $DIR/../helpers
+source $DIR/os/$(machine_os)
 create_symlink $DIR/conf ~/.tmux.conf
 
 # __END__
