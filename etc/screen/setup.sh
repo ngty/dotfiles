fad3da91b5811fa60735560d2096194e565ba171
Ng Tze Yang <ngty77@gmail.com>
2012-01-18 23:16:04 +0800

    Make it even more convenient to setup screen.

--- etc/screen/setup.sh
+++ etc/screen/setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-source `dirname $0`/../helpers
-
 DIR=`dirname ${BASH_SOURCE[0]}`
 DIR=`cd $DIR && pwd`
+
+source $DIR/../helpers
+source $DIR/os/$(machine_os)
 
 create_symlink $DIR/basic ~/.screenrc
 
